b835ddd7079f4acff6bb845fe8b6d9e4a24fbbc8
Mitushi Ananya <51473813+mitushiananya@users.noreply.github.com>
2020-12-28 01:55:12 +0530

    R Script

---
+++ tweetscovid19analysis.R
@@ -0,0 +1,92 @@
+# Load the required Packages
+library(twitteR)
+library(RCurl)
+library(httr)
+library(tm)
+library(wordcloud)
+library(syuzhet)
+library(plotly)
+
+consumer_key = "Yy3Yl01EY3NQWMUbmJM5SqQhd"
+consumer_secret = "BiDYjPG22tJljKnYmON3tpuwpMdYCHtJK6fbHkgXqOMQiDHYnh"
+access_token = "1224344141401817089-D8KfBvhBqjhchFRXNp0yFk7BZXPMAP"
+access_secret = "0HP7tc48fuPTEUsNtg7uBlGr2eT2GrkLooKrZXKgjtuiq"
+
+# set up
+setup_twitter_oauth(consumer_key,consumer_secret,access_token, access_secret)
+
+# search for tweets in english language
+tweets = searchTwitter("#COVID-19", n = 10000, lang = "en")
+
+# store the tweets into dataframe
+tweets.df = twListToDF(tweets)
+tweets.df
+
+# CLEANING TWEETS
+
+tweets.df$text=gsub("&amp", "", tweets.df$text)
+tweets.df$text = gsub("&amp", "", tweets.df$text)
+tweets.df$text = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweets.df$text)
+tweets.df$text = gsub("@\\w+", "", tweets.df$text)
+tweets.df$text = gsub("[[:punct:]]", "", tweets.df$text)
+tweets.df$text = gsub("[[:digit:]]", "", tweets.df$text)
+tweets.df$text = gsub("http\\w+", "", tweets.df$text)
+tweets.df$text = gsub("[ \t]{2,}", "", tweets.df$text)
+tweets.df$text = gsub("^\\s+|\\s+$", "", tweets.df$text)
+
+tweets.df$text <- iconv(tweets.df$text, "UTF-8", "ASCII", sub="")
+tweets.df$text
+
+# Emotions for each tweet using NRC dictionary
+emotions <- get_nrc_sentiment(tweets.df$text)
+emo_bar = colSums(emotions)
+emo_sum = data.frame(count=emo_bar, emotion=names(emo_bar))
+emo_sum$emotion = factor(emo_sum$emotion, levels=emo_sum$emotion[order(emo_sum$count, decreasing = TRUE)])
+emo_sum$emotion
+emo_sum$count
+
+# Visualize the emotions from NRC sentiments
+p <- plot_ly(emo_sum, x=~emotion, y=~count, type="bar", color=~emotion) %>%
+  layout(xaxis=list(title="Emotions"), showlegend=FALSE,
+         title="Emotion Type for hashtag: #Covid-19")
+
+p
+
+# Create comparison word cloud data
+
+wordcloud_tweet = c(
+  paste(tweets.df$text[emotions$anger > 0], collapse=" "),
+  paste(tweets.df$text[emotions$anticipation > 0], collapse=" "),
+  paste(tweets.df$text[emotions$disgust > 0], collapse=" "),
+  paste(tweets.df$text[emotions$fear > 0], collapse=" "),
+  paste(tweets.df$text[emotions$joy > 0], collapse=" "),
+  paste(tweets.df$text[emotions$sadness > 0], collapse=" "),
+  paste(tweets.df$text[emotions$surprise > 0], collapse=" "),
+  paste(tweets.df$text[emotions$trust > 0], collapse=" ")
+)
+
+# create corpus
+corpus = Corpus(VectorSource(wordcloud_tweet))
+
+# remove punctuation, convert every word in lower case and remove stop words
+
+corpus = tm_map(corpus, tolower)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, c(stopwords("english")))
+corpus = tm_map(corpus, stemDocument)
+
+# create document term matrix
+
+tdm = TermDocumentMatrix(corpus)
+
+# convert as matrix
+tdm = as.matrix(tdm)
+tdmnew <- tdm[nchar(rownames(tdm)) < 11,]
+
+# column name binding
+colnames(tdm) = c('anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust')
+colnames(tdmnew) <- colnames(tdm)
+comparison.cloud(tdmnew, random.order=FALSE,
+                 colors = c("#00B2FF", "red", "#FF0099", "#6600CC", "green", "orange", "blue", "brown"),
+                 title.size=1, max.words=250, scale=c(2.5, 0.4),rot.per=0.4)
+
